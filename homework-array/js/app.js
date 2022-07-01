26511945f8c600b105eaf8b9e6333d230c072ca2
barshani <106982986+barshani@users.noreply.github.com>
2022-07-01 15:54:44 +0300

    Delete app.js

--- homework-array/js/app.js
+++
@@ -1,9 +0,0 @@
-let numArr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-let shapeArr = ['hearts','diamonds','spades','clubs'];
-let y = 0;
-for(let x = 0; x<numArr.length; x++){
-    console.log(numArr[x]," of ",shapeArr[y++]);
-    if(y===4){
-        y=0;
-    }
-}
